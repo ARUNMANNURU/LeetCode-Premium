2cc2f70d1de25d5484f1774c96dc63b9c169394f
Mannuru <arunm057@gmail.com>
2018-10-20 12:33:51 -0700

    Commiting list problems

---
+++ src/introduction/linkedlist/problems/Node.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package introduction.linkedlist.problems;
+
+/**
+ * @author ArunMannuru
+ *
+ */
+public class Node {
+	int val;
+	Node next;
+	Node(int x){
+		val = x;
+		next = null;
+	}
+}
